24cb8d32f8d486d26a62b8cfa6a8efbd906c4df6
raulariias <raularias24685@gmail.com>
2023-07-04 11:09:31 -0400

    Feat: Creación del navbar y el Footer

--- front/src/layout/Layout.jsx
+++ front/src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
 
 export const Layout = () => {
   return (
     <div>
-      <div>Header</div>
+      <Header />
       <Outlet />
-      <div>Footer</div>
+      <Footer />
     </div>
   )
 }
