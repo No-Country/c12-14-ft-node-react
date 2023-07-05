5b67d909a9a937764f044ccde436dc7da4742fe2
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-05 15:44:15 -0500

    feat: Add ProjectModal component to AppRoutes

--- front/src/router/routes.jsx
+++ front/src/router/routes.jsx
@@ -2,10 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Layout } from '@/layout/Layout'
 import Home from './Home/Home'
 import Landing from './Landing/Landing'
+import { ProjectModal } from '../components/ProjectModal/ProjectModal'
+import { useSelector } from 'react-redux'
 
 export const AppRoutes = () => {
+  const { isOpen } = useSelector((state) => state.projectModal)
   return (
     <BrowserRouter>
+      {isOpen && <ProjectModal />}
       <Routes>
         <Route element={<Layout />}>
           <Route path='/' element={<Home />} />
