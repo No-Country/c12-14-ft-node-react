f3bf56cd9ed34c43c82bf823f256fb4569ef48d0
Jefersonsteven <gfreycode@gmail.com>
2023-07-11 12:59:12 -0500

    add

--- back/config/http/server.js
+++ back/config/http/server.js
@@ -1,42 +1,32 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
-const mainRoutes = require('../../src/routes/main');
-const userRoutes = require('../../src/routes/users');
+const mainRoutes = require("../../src/routes/main");
+const userRoutes = require("../../src/routes/users");
 
 class Server {
-
   constructor() {
-
     this.app = express();
     this.port = process.env.PORT || 3000;
     this.middlewares();
     this.routes();
-
   }
 
   routes() {
-
-    this.app.use('/', mainRoutes);
-    this.app.use('/users', userRoutes);
-
+    this.app.use("/", mainRoutes);
+    this.app.use("/users", userRoutes);
   }
 
   middlewares() {
-
     this.app.use(cors());
     this.app.use(express.json());
-
   }
 
   listen() {
-
     this.app.listen(this.port, () => {
       console.log(`[server-info]:Server up at http://127.0.0.1:${this.port}`);
     });
   }
-
-
 }
 
 module.exports = Server;
