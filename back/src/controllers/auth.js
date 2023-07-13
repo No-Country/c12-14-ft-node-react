ac88f92affe2d94c467d9539e19c90c163887d2d
Jefersonsteven <gfreycode@gmail.com>
2023-07-13 13:31:57 -0500

    feat: auth login logout and register

--- back/src/controllers/auth.js
+++ back/src/controllers/auth.js
@@ -1,152 +1,152 @@
-const user = require("../repositories/user");
-const userRepository = require("../repositories/user");
-const AuthServices = require("../services/auth.service");
-const { serialize } = require("cookie");
+const user = require('../repositories/user')
+const userRepository = require('../repositories/user')
+const AuthServices = require('../services/auth.service')
+const { serialize } = require('cookie')
 
 const register = async (req, res = response) => {
-  const { username, mail, password, phonenumber } = req.body;
-  const authServices = new AuthServices();
+  const { username, mail, password, phonenumber } = req.body
+  const authServices = new AuthServices()
 
   try {
-    const userEmail = await userRepository.findUserByEmail(mail);
-    const userName = await userRepository.findUserByUsername(username);
+    const userEmail = await userRepository.findUserByEmail(mail)
+    const userName = await userRepository.findUserByUsername(username)
 
     if (userEmail || userName) {
       return res.status(400).json({
-        msg: "User already exists",
-      });
+        msg: 'User already exists',
+      })
     }
 
-    const hash = await authServices.encryptPassword(password);
+    const hash = await authServices.encryptPassword(password)
     const newUser = await userRepository.create({
       userName: username,
       mail: mail,
       password: hash,
-    });
+    })
 
-    res.status(200).json({ msg: "User created", newUser });
+    res.status(200).json({ msg: 'User created', newUser })
   } catch (error) {
     res.status(500).json({
-      msg: "Error in register",
+      msg: 'Error in register',
       error: error.message,
-    });
+    })
   }
-};
+}
 
 const login = async (req, res = response) => {
-  const { username, mail, password } = req.body;
-  const authServices = new AuthServices();
+  const { username, mail, password } = req.body
+  const authServices = new AuthServices()
 
   try {
-    let user;
+    let user
 
     if (username.length === 0) {
-      user = await userRepository.findUserByEmail(mail);
+      user = await userRepository.findUserByEmail(mail)
     } else {
-      user = await userRepository.findUserByUsername(username);
+      user = await userRepository.findUserByUsername(username)
     }
 
     if (!user) {
       return res.status(400).json({
-        msg: "User does not exist",
-      });
+        msg: 'User does not exist',
+      })
     }
 
-    const isMatch = await authServices.comparePassword(password, user.password);
+    const isMatch = await authServices.comparePassword(password, user.password)
 
     if (!isMatch) {
       return res.status(400).json({
-        msg: "Incorrect password",
-      });
+        msg: 'Incorrect password',
+      })
     } else {
-      console.log(user);
+      console.log(user)
       const token = await authServices.generateJWT({
         id: user.id,
         username: user.userName,
         mail: user.mail,
-      });
+      })
 
-      const serialized = serialize("devCollabToken", token, {
+      const serialized = serialize('devCollabToken', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== "production",
-        sameSite: "none",
+        secure: process.env.NODE_ENV !== 'production',
+        sameSite: 'none',
         maxAge: 60 * 60 * 24,
-      });
+      })
 
-      res.setHeader("Set-Cookie", serialized);
+      res.setHeader('Set-Cookie', serialized)
 
       res.status(200).json({
-        msg: "Login success",
-        session: "created",
+        msg: 'Login success',
+        session: 'created',
         token,
-      });
+      })
     }
   } catch (error) {
     res.status(500).json({
-      msg: "Error in login",
+      msg: 'Error in login',
       error: error.message,
-    });
+    })
   }
-};
+}
 
 const logout = async (req, res = response) => {
   try {
     // const token = req.cookies.devCollabToken;
-    const token = req.body.devCollabToken;
+    const token = req.body.devCollabToken
     if (token) {
-      const authServices = new AuthServices();
+      const authServices = new AuthServices()
       try {
-        authServices.verifyJWT(token);
-        const serialized = serialize("devCollabToken", token, {
+        authServices.verifyJWT(token)
+        const serialized = serialize('devCollabToken', token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV !== "production",
-          sameSite: "none",
+          secure: process.env.NODE_ENV !== 'production',
+          sameSite: 'none',
           maxAge: 0,
-        });
-        res.setHeader("Set-Cookie", serialized);
+        })
+        res.setHeader('Set-Cookie', serialized)
         res.status(200).json({
-          msg: "Logout success",
-          session: "destroyed",
-        });
+          msg: 'Logout success',
+          session: 'destroyed',
+        })
       } catch (error) {}
     }
   } catch (error) {
     res.status(500).json({
-      msg: "Error in logout",
+      msg: 'Error in logout',
       error: error.message,
-    });
+    })
   }
-};
+}
 
 const verify = async (req, res = response) => {
   try {
     // const token = req.cookies.devCollabToken;
-    const token = req.body.devCollabToken;
+    const token = req.body.devCollabToken
     if (token) {
-      const authServices = new AuthServices();
+      const authServices = new AuthServices()
       const {
         user: { id, username, mail },
-      } = await authServices.verifyJWT(token);
+      } = await authServices.verifyJWT(token)
       res.status(200).json({
-        msg: "User verified",
+        msg: 'User verified',
         user: {
           id,
           username,
           mail,
         },
-      });
+      })
     }
   } catch (error) {
     res.status(500).json({
-      msg: "Error in verify",
+      msg: 'Error in verify',
       error: error.message,
-    });
+    })
   }
-};
+}
 
 module.exports = {
   register,
   login,
   verify,
   logout,
-};
+}
