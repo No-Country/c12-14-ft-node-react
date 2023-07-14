c16ce4c2700e05504dfb484cd80d2c537956301c
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-14 14:43:46 -0500

    feat(register-form): add form submission logic

--- front/src/components/Auth/RegisterForm/RegisterForm.jsx
+++ front/src/components/Auth/RegisterForm/RegisterForm.jsx
@@ -1,7 +1,37 @@
 import { Link } from 'react-router-dom'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '@/firebase/firebase.js'
 import { RegisterInput } from './RegisterInput'
+import { useState } from 'react'
 
 const RegisterForm = () => {
+  const [errors, setErrors] = useState({
+    mail: '',
+    username: '',
+    password: '',
+  })
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    const { mail, username, password } = e.target.elements
+
+    // TODO: Validar los campos
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(
+        auth,
+        mail.value,
+        password.value
+      )
+      // TODO: Crear el usuario en backend con el id de firebase y el username
+      // TODO: Guardar el usuario en el estado global
+      // TODO: Redireccionar al usuario a la pagina de inicio
+      console.log(userCredentials)
+    } catch (error) {
+      // TODO: Mostrar el error al usuario en el input
+      console.log(error)
+    }
+  }
+
   return (
     <div className='m-5 bg-white w-[539px] p-10 rounded-xl flex flex-col gap-10 formShadow'>
       <section className='flex w-full justify-between'>
@@ -19,12 +49,13 @@ const RegisterForm = () => {
         <h1 className='text-4xl font-bold'>Registrarme</h1>
       </section>
 
-      <form className='flex flex-col gap-7 w-full'>
+      <form className='flex flex-col gap-7 w-full' onSubmit={onSubmit}>
         <RegisterInput
           label='Email'
           name='mail'
           type='email'
           placeholder='Correo electrónico'
+          errors={errors.mail}
         />
 
         <div className='flex gap-4'>
@@ -33,6 +64,7 @@ const RegisterForm = () => {
             name='username'
             type='text'
             placeholder='Usuario'
+            errors={errors.username}
           />
 
           <RegisterInput
@@ -48,6 +80,7 @@ const RegisterForm = () => {
           name='password'
           type='password'
           placeholder='Contraseña'
+          errors={errors.password}
         />
 
         <RegisterInput
