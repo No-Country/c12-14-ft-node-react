686d7718fc3547b5af3a0f5cc629f85534a19ffb
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-16 09:37:44 -0500

    style: modify tailwind.config.js colors

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#6C33F2',
+        primaryDark: '#29278D',
+        secondaryBackground: '#FAFAFA',
+        secondaryContainer: '#FFFFFF',
+        secondaryNav: '#FEFDFD',
+        secondaryContainerBorder: '#5526B366',
+        secondaryText: '#323238',
+      },
+    },
   },
   plugins: [],
 }
