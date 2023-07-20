5deb6b13173333ebd3ea3889aceec223e6ee2f86
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-07-20 07:40:42 -0500

    style(ProjectModal): improve code formatting and organization

--- front/src/components/ProjectModal/ProjectModal.jsx
+++ front/src/components/ProjectModal/ProjectModal.jsx
@@ -1,11 +1,12 @@
 import { GrStatusGoodSmall } from 'react-icons/gr'
-import { MdArrowBackIos, MdOutlineDateRange } from 'react-icons/md'
+import { MdArrowBackIos, MdDateRange } from 'react-icons/md'
 import { LuClock4 } from 'react-icons/lu'
 import {
   BsFillExclamationOctagonFill,
   BsFillShareFill,
   BsFillTrophyFill,
 } from 'react-icons/bs'
+import { TbWorld } from 'react-icons/tb'
 import { BiSolidUser } from 'react-icons/bi'
 import { IoPersonAddSharp } from 'react-icons/io5'
 import { HiUsers } from 'react-icons/hi'
@@ -18,138 +19,138 @@ export const ProjectModal = ({ project, setShowModal }) => {
 
   return (
     <div
-      className='fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50 backdrop-blur-sm'
+      className='fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm'
       onClick={() => setShowModal(false)}
     >
       <div
-        className='flex gap-5 text-white relative'
+        className='relative flex gap-5 text-white'
         onClick={(e) => e.stopPropagation()}
       >
         <MdArrowBackIos
-          className='absolute text-5xl -top-14 left-0 text-white  cursor-pointer'
+          className='absolute -top-14 left-0 cursor-pointer text-5xl  text-white'
           onClick={() => setShowModal(false)}
         />
-        <div className='flex flex-col gap-5 w-[680px]'>
-          <div className='flex bg-gray-600 rounded-lg items-center justify-between py-8 px-10 text-2xl'>
-            <h4>Titulo de proyecto</h4>
-            <div className='flex items-center gap-2 text-sm'>
-              <GrStatusGoodSmall />
+        <div className='flex w-[680px] flex-col gap-5 text-primaryDark'>
+          <div className='flex items-center justify-between rounded-lg border-2 border-secondaryContainerBorder bg-white px-10 py-8 text-2xl'>
+            <h4 className='font-extrabold'>Titulo de proyecto</h4>
+            <div className='flex items-center gap-2 text-sm font-semibold'>
+              <GrStatusGoodSmall className='text-green-400' />
               Activo
             </div>
           </div>
 
           <div>
-            <div className='bg-gray-600 rounded-t-lg border-b-2 w-full flex flex-col py-8 px-10'>
-              <div className='flex items-center justify-between text-lg'>
-                <div className='font-bold'>Categoria</div>
-                <div className='flex items-center gap-3 text-sm'>
-                  <MdOutlineDateRange className='text-xl' />
-                  Fecha de comienzo
+            <div className='flex w-full flex-col rounded-t-lg border-2 border-b-0 border-secondaryContainerBorder bg-white px-10 py-8'>
+              <div className='flex items-center justify-between'>
+                <div className='font-bold'>
+                  Categoría:
+                  <span className='font-semibold text-secondaryText'>
+                    {' '}
+                    Comercio
+                  </span>
+                </div>
+
+                <div className='text-semibold flex flex-col items-center gap-1 text-sm font-semibold '>
+                  <div className='flex items-center gap-3'>
+                    <MdDateRange className='text-lg' />
+                    Fecha de comienzo
+                  </div>
+                  <span className='text-secondaryText'>01-Sept-2023</span>
                 </div>
               </div>
-              <div className='flex items-center gap-2 text-sm pt-8 pb-4'>
-                <GrStatusGoodSmall />
-                Idioma
+              <div className='flex items-center gap-2 py-2 font-bold'>
+                <TbWorld className='text-lg' />
+                Idioma:
+                <span className='font-medium text-secondaryText'> Español</span>
               </div>
             </div>
-            <div className='bg-gray-600 w-full flex flex-col py-8 px-10'>
+            <div className='flex w-full flex-col border-2 border-secondaryContainerBorder bg-white px-10 py-8'>
               <div>
-                <h4 className='font-semibold text-sm mb-1'>
+                <h4 className='mb-1 pb-6 text-sm font-bold'>
                   Descripción de este proyecto:
                 </h4>
-                <div className='text-sm pr-28 text-justify leading-6'>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Deleniti aut, unde commodi vero nam reprehenderit repudiandae
-                  ea nostrum accusantium perspiciatis quis veniam illo,
-                  laudantium reiciendis aspernatur mollitia perferendis fugit
-                  nemo.
-                  <ul>
-                    <li>- Lorem ipsum dolor sit / amet consectetur.</li>
-                    <li>
-                      - In arcu nunc enim amet amet faucibus / in egestas.
-                    </li>
-                    <li>
-                      - morbi sollicitudin. Convallis odio lacus enim lobortis
-                      lacinia.
-                    </li>
-                    <li>- Lorem ipsum dolor sit / amet consectetur.</li>
-                    <li>
-                      - In arcu nunc enim amet amet faucibus / in egestas.
-                    </li>
-                    <li>
-                      - morbi sollicitudin. Convallis odio lacus enim lobortis
-                      lacinia.
-                    </li>
-                    <li>- Lorem ipsum dolor sit / amet consectetur.</li>
-                    <li>
-                      - In arcu nunc enim amet amet faucibus / in egestas.
-                    </li>
-                    <li>
-                      - morbi sollicitudin. Convallis odio lacus enim lobortis
-                      lacinia.
-                    </li>
-                  </ul>
+                <div className='flex flex-col gap-6 text-justify text-sm font-medium leading-6 text-secondaryText'>
+                  <p>
+                    Desarrollo de una aplicación de compras en línea que permite
+                    a los usuarios explorar y comprar productos, administrar su
+                    carrito de compras y realizar pagos seguros.
+                  </p>
+                  <p>
+                    Ofrecerá una interfaz intuitiva, amplio catálogo de
+                    productos, opciones de pago seguras y entrega rápida.
+                    Impulsará la experiencia de compra con funciones de
+                    seguimiento de pedidos y soporte al cliente, brindando una
+                    solución completa para compradores y vendedores en el
+                    mercado digital actual
+                  </p>
                 </div>
               </div>
             </div>
-            <div className='bg-gray-600 rounded-b-lg w-full text-sm flex flex-col p-10 pt-0'>
+            <div className='flex w-full flex-col rounded-b-lg border-2 border-t-0 border-secondaryContainerBorder bg-white p-10 pt-20 text-sm'>
               <div className='flex items-center justify-between font-bold'>
-                <div className='flex items-center gap-4'>
-                  <LuClock4 className='text-lg' />
-                  Duración aproximada
+                <div className='flex flex-col gap-2'>
+                  <div className='flex items-center gap-4'>
+                    <LuClock4 className='text-lg' />
+                    Duración aproximada
+                  </div>
+                  <span className='ml-8 font-semibold text-secondaryText'>
+                    Cuatro semanas
+                  </span>
                 </div>
-                <div className='flex items-center gap-4'>
+                <div className='flex items-center gap-4 self-start'>
                   <BsFillTrophyFill />
-                  Seniority Requerida
+                  Perfiles Junior
                 </div>
               </div>
-              <div className='flex items-center gap-1 pt-10 pb-2'>
+              <div className='flex items-center gap-4 pb-2 pt-10'>
                 <BiSolidUser className='text-lg' />
-                Publicado por <span className='font-semibold'>User test</span>
+                <p>
+                  Publicado por <span className='font-semibold'>User test</span>
+                </p>
               </div>
             </div>
           </div>
         </div>
 
-        <div className='bg-gray-600 rounded-lg w-80 flex flex-col'>
-          <div className='flex border-b-2 flex-col gap-5 items-center justify-between p-10 font-bold text-xs'>
-            <button className='bg-gray-800 w-full py-4 rounded-md'>
+        <div className='flex w-80 flex-col rounded-lg bg-white font-bold text-primary'>
+          <div className='flex flex-col items-center justify-between gap-5 rounded-t-md border-2  border-b-0 border-secondaryContainerBorder p-10 pb-6 font-bold'>
+            <button className='w-full rounded-md bg-primary py-3 text-white'>
               Aplicar
             </button>
-            <button className='border rounded-md py-4 w-full'>
+            <button className='w-full rounded-md border-2 border-primary border-secondaryContainerBorder py-3'>
               Guardar en favoritos
             </button>
-            <div className='flex self-start pt-7 items-center gap-2 cursor-pointer font-normal'>
+            <div className='flex cursor-pointer items-center gap-8 self-start pt-7 font-semibold'>
               <BsFillExclamationOctagonFill className='text-lg' />
               Reportar este proyecto
             </div>
           </div>
-          <div className='flex flex-col p-10 py-14 text-sm border-b-2'>
-            <div className='flex pb-2 items-center gap-2 font-normal'>
+          <div className='flex flex-col border-2 border-secondaryContainerBorder p-10 py-14 text-sm'>
+            <div className='flex items-center gap-2 pb-2 font-semibold'>
               <IoPersonAddSharp />
               Perfiles vacantes:
             </div>
-            <ul className='list-disc pl-14 flex flex-col gap-1'>
+            <ul className='flex list-disc flex-col gap-1 pl-14 text-secondaryText'>
               <li>(2) Frontend</li>
               <li>(1) Scrum Master</li>
               <li>(2) QA Tester</li>
               <li>(1) Product Owner</li>
             </ul>
 
-            <div className='flex pt-7 items-center gap-2 font-normal'>
+            <div className='flex items-center gap-2 pt-7 font-semibold'>
               <HiUsers className='text-lg' />
               Equipo de 12 integrantes
             </div>
           </div>
 
-          <div className='flex flex-col p-10 py-14 text-sm'>
-            <div className='flex items-center gap-2 pb-8 font-normal'>
+          <div className='flex flex-col rounded-b-md border-2 border-t-0 border-secondaryContainerBorder p-10 py-14 text-sm'>
+            <div className='flex items-center gap-2 pb-8 font-bold'>
               <BsFillShareFill />
               Compartir
             </div>
 
             <input
-              className='rounded-md py-3 text-neutral-800 font-bold px-4 mt-2 cursor-pointer'
+              className='mt-2 cursor-pointer rounded-md bg-primary bg-opacity-10 px-4 py-3 font-bold text-primary'
               type='text'
               value={copyToClipboard}
               readOnly
@@ -162,7 +163,7 @@ export const ProjectModal = ({ project, setShowModal }) => {
               }}
             />
             <p
-              className='underline cursor-pointer pt-4'
+              className='cursor-pointer pt-4 underline'
               onClick={() => {
                 navigator.clipboard.writeText(copyToClipboard)
                 setCopyToClipboard('Copiado!')
