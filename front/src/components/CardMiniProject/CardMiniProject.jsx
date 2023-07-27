767d6f6af1ddf2d56308d34e263cce781fd75e89
Jefersonsteven <gfreycode@gmail.com>
2023-07-27 00:58:55 -0500

    refactor: landing

--- front/src/components/CardMiniProject/CardMiniProject.jsx
+++ front/src/components/CardMiniProject/CardMiniProject.jsx
@@ -1,16 +1,23 @@
+import { useNavigate } from 'react-router-dom'
 function CardMiniProject({ project }) {
+  const navigate = useNavigate()
+  function handleClick() {
+    return navigate(`/project/${project._id}`)
+  }
   return (
-    <div className='flex flex-col gap-5 p-5 bg-white text-black rounded-2xl'>
-      <div className='flex justify-between'>
-        <h4 className='text-2xl'>{project.title}</h4>
-        <div className='flex justify-between gap-2 items-center'>
-          <div className=' w-2 h-2 bg-[green] rounded-full'></div>
-          <p>{project.status}</p>
+    <div className='flex flex-col gap-5 rounded-2xl bg-white p-5 capitalize text-black'>
+      <div className='flex justify-between gap-1'>
+        <h4 className='textwrap cursor-pointer' onClick={handleClick}>
+          {project.title}
+        </h4>
+        <div className='flex min-w-[60%] items-center justify-end gap-2'>
+          <div className=' h-2 w-2 rounded-full bg-[green]'></div>
+          <p className=' text-right'>{project.status}</p>
         </div>
       </div>
-      <p className=' w-3/4'>{project.description}</p>
-      <div className='flex gap-2 items-center text-[#0089ED] font-bold'>
-        <div className=' w-8 h-8 rounded-full bg-[#0089ED]'></div>
+      <p className=' textwrap w-3/4'>{project.description}</p>
+      <div className='flex items-center gap-2 font-bold text-[#0089ED]'>
+        <div className=' h-4 w-4 rounded-full bg-[#0089ED]'></div>
         <p>{project.category}</p>
       </div>
     </div>
