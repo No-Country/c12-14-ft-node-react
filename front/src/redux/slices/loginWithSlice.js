ca607417c03e605c03ebea9cd3c2a24b63447a9e
Jefersonsteven <gfreycode@gmail.com>
2023-07-26 22:08:51 -0500

    refactor: login form

--- front/src/redux/slices/loginWithSlice.js
+++ front/src/redux/slices/loginWithSlice.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
 } from 'firebase/auth'
 import { auth } from '../../firebase/firebase'
+import { uvaApi } from '../../api/index'
 
 const initialState = {
   login: {},
@@ -19,8 +20,21 @@ const loginWithSlice = createSlice({
         try {
           const googleProvider = new GoogleAuthProvider()
           const access = await signInWithPopup(auth, googleProvider)
+
+          const credential = GoogleAuthProvider.credentialFromResult(access)
+          const token = credential.accessToken
+          const body = {
+            username: access.user.displayName,
+            email: access.user.email,
+            password: crypto.randomUUID(),
+            picture: access.user.photoURL,
+            token: token | access.user.accessToken,
+          }
+          if (access.user.emailVerified) {
+            const login = uvaApi.post('/auth/google/register', body)
+            console.log(body, login, access, token)
+          }
           // TODO: add logic to save user data in redux
-          console.log('proof loginGoogle', access)
         } catch (error) {
           console.log(error.message)
         }
@@ -33,7 +47,19 @@ const loginWithSlice = createSlice({
         try {
           const githubProvider = new GithubAuthProvider()
           const access = await signInWithPopup(auth, githubProvider)
+          const body = {
+            username: access.user.displayName,
+            email: access.user.email,
+            password: crypto.randomUUID(),
+            picture: access.user.photoURL,
+            token: access._tokenResponse.oauthAccessToken,
+          }
+          if (access.user.emailVerified) {
+            const login = uvaApi.post('/auth/github/register', body)
+            console.log(body, login, access)
+          }
           // TODO: add logic to save user data in redux
+
           console.log('proof loginGithub', access)
         } catch (error) {
           console.log(error.message)
